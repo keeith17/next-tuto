65dc4246bb16eafef97ccad6df042c5ff0871691
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-07-30 22:55:58 +0900

    임시 저장 home 화면까지

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import mainImage from 'public/main.jpeg'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the global tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={mainImage} alt="mainImage" className={styles.img}/>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>@2023 gabin. All rights reserved.</div>
+      <div>
+        <Image src="" alt="gabin portfolio"/>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
