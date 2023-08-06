541757a37ee148fccad8d9a9891499da823ef586
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-08-06 23:04:16 +0900

    0807 portfolio uiux까지

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/Button/Button'
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's keep in Touch</h1>
+      <div className={styles.content}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/7784599/pexels-photo-7784599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          fill={true}
+          alt=""
+          className={styles.image}
+        />
+      </div>
+      <form className={styles.form}>
+        <input type="text" placeholder='name' className={styles.input} />
+        <input type="text" placeholder='email' className={styles.input} />
+        <textarea className={styles.textArea} placeholder='message' cols="30" rows="10"></textarea>
+        <Button url="#" text="send"/>
+      </form>
+      </div>
+    </div>
   )
 }
 
