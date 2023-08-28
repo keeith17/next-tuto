3e2598f639f8a3c0166bfe165c43c47cfb2dc769
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-08-28 22:33:19 +0900

    이게 되나

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 import { notFound } from "next/navigation"
 
 async function getData(id) {
-  const res = await fetch(`http://0.0.0.0:3000/api/posts/${id}`, {
+  const res = await fetch(`https://next-tuto-one.vercel.app/api/posts/${id}`, {
     next: { revalidate: 10 },
     // cache: 'no-store'
   });
