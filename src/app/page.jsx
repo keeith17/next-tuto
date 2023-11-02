9a69a5243b0b7338fe0d95b456e604f773871d74
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-11-03 02:34:43 +0900

    포트폴리오 변경

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,59 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import mainImage from 'public/main.jpeg'
-import Button from '@/components/Button/Button'
+import Image from "next/image";
+import styles from "./page.module.css";
+import mainImage from "public/main.jpeg";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <h1 className={styles.title}>
-          Better design for your digital products.
-        </h1>
-        <p className={styles.desc}>
-          Turning your Idea into Reality. We bring together the teams from the global tech industry.
-        </p>
-        <Button url="/portfolio" text="See our works"/>
-      </div>
-      <div className={styles.item}>
-        <Image src={mainImage} alt="mainImage" className={styles.img}/>
-      </div>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <div>
+                    <ul className={styles.title}>
+                        <li className={styles.greet}>안녕하세요!</li>
+                        <li className={styles.intro}>열려 있는 마음으로</li>
+                        <li className={styles.intro}>
+                            유연하게 사고하는 개발자
+                        </li>
+                    </ul>
+                    <div className={styles.intro}>
+                        <span className={styles.myName}>염가빈</span>입니다.
+                    </div>
+                </div>
+
+                <Button url="/portfolio" text="포트폴리오 보러 가기" />
+            </div>
+            <div className={styles.itemRight}>
+                <div className={styles.itemBox}>
+                    <div className={styles.sTitle}>기술 스택</div>
+                    <div className={styles.content}>
+                        <p>HTML5, CSS3, jQuery</p>
+                        <p>javaScript, typeScript, PHP, Firebase</p>
+                        <p>React.js, Next.js</p>
+                    </div>
+                </div>
+                <div className={styles.itemBox}>
+                    <div className={styles.sTitle}>자격증</div>
+                    <div className={styles.content}>
+                        <p>
+                            웹디자인 기능사
+                            <span className={styles.date}>2022-09</span>
+                        </p>
+                        <p>
+                            정보처리기사
+                            <span className={styles.date}>2023-09</span>
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.contactBox}>
+                    <p className={styles.contact}>
+                        github: https://github.com/keeith17
+                    </p>
+                    <p className={styles.contact}>
+                        Email: paranoiaque17@gmail.com
+                    </p>
+                </div>
+                {/* <Image src={mainImage} alt="mainImage" className={styles.img} /> */}
+            </div>
+        </div>
+    );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,67 +1,65 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import styles from './navbar.module.css'
-import DarkModeToggle from '../DarkModeToggle/DarkModeToggle'
-import { signOut, useSession } from 'next-auth/react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
-  },
-  {
-    id: 3,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-  {
-    id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
-  },
-]
+    {
+        id: 1,
+        title: "홈",
+        url: "/",
+    },
+    {
+        id: 2,
+        title: "포트폴리오",
+        url: "/portfolio",
+    },
+    {
+        id: 3,
+        title: "방명록",
+        url: "/blog",
+    },
+    // {
+    //   id: 4,
+    //   title: "About",
+    //   url: "/about",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Contact",
+    //   url: "/contact",
+    // },
+    {
+        id: 6,
+        title: "내가 쓴 글",
+        url: "/dashboard",
+    },
+];
 
 const Navbar = () => {
-  const session = useSession();
-  return (
-    <div className={styles.container}>
-      <Link href="/" className={styles.logo}>
-        gabin portfolio
-      </Link>
-      <div className={styles.links}>
-        <DarkModeToggle />
-        {links.map(link=>(
-          <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
-        ))}
-        {session.status === "authenticated" && (
-          <button
-          className={styles.logout}
-          onClick={signOut}
-          >
-          Logout
-        </button>
-        )
-        }
-      </div>
-    </div>
-  )
-}
+    const session = useSession();
+    return (
+        <div className={styles.container}>
+            <Link href="/" className={styles.logo}>
+                gabin portfolio
+            </Link>
+            <div className={styles.links}>
+                <DarkModeToggle />
+                {links.map((link) => (
+                    <Link key={link.id} href={link.url} className={styles.link}>
+                        {link.title}
+                    </Link>
+                ))}
+                {session.status === "authenticated" && (
+                    <button className={styles.logout} onClick={signOut}>
+                        Logout
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
